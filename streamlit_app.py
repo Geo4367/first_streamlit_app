1142b02e9b5d1e8a08fa7d0dbe1a3f646c4d39a2
GEORGEKUTTY ANTONY <86464328+Geo4367@users.noreply.github.com>
2023-11-25 17:27:38 +0000

    Update streamlit_app.py
    nothing

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket smothie')
 streamlit.text('Hard Bolied Free range egg')
+
