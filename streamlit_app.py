b05ff0bf6296017417757cf5c5942d2b75a5571e
GEORGEKUTTY ANTONY <86464328+Geo4367@users.noreply.github.com>
2023-11-27 20:33:23 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
